278114ce1da6e4de99717409e036014dedb16b25
hamza-140 <sahamzashah19@gmail.com>
2024-05-05 03:07:12 +0500

    Add existing project files to Git

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -46,7 +46,10 @@ function Calculator() {
         <button onClick={() => handleClick('/')}>/</button>
       </div>
       <div>
-        <button style={{width:"190px"}} onClick={() => handleClick('C')}>C</button>
+      <button  style={{width:"190px"}} onClick={() => handleClick('C')} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+<span  style={{width:"190px"}} class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+C</span>
+</button>
       </div>
     </div>
   );

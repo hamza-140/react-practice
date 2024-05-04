325c5e4e8c6fa49597ab03a0b4e9bfd6faa5cc0e
hamza-140 <sahamzashah19@gmail.com>
2024-05-05 03:17:27 +0500

    Add existing project files to Git

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -73,15 +73,23 @@ function Timer() {
           disabled={isActive}
         />
       </div>
-      <button onClick={handleStart} disabled={isActive || (!inputMinutes && !inputSeconds)}>
-        Start
-      </button>
-      <button onClick={handlePause} disabled={!isActive}>
+      
+      <button onClick={handleStart} disabled={isActive || (!inputMinutes && !inputSeconds)} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400" onClick={() => handleClick('7')}>
+    <span   className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Start
+    </span>
+  </button>
+  <button onClick={handlePause} disabled={!isActive} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400" onClick={() => handleClick('7')}>
+    <span   className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
         Pause
-      </button>
-      <button onClick={handleReset}>
-        Reset
-      </button>
+    </span>
+  </button><button onClick={handleReset} disabled={!isActive} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400" onClick={() => handleClick('7')}>
+    <span   className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Reset
+    </span>
+  </button>
+      
+     
     </div>
   );
 }

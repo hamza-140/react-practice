d89473b518e5c599a67c5b048e3d4e99c7ee87bb
hamza-140 <sahamzashah19@gmail.com>
2024-05-05 03:45:28 +0500

    Add existing project files to Git

--- src/UploadImage.jsx
+++ src/UploadImage.jsx
@@ -8,6 +8,10 @@ function UploadImage() {
     setImage(selectedImage);
   };
 
+  const removeImage = () => {
+    setImage(null);
+  };
+
   return (
     <div className="max-w-lg mx-auto my-8">
       <input
@@ -24,6 +28,14 @@ function UploadImage() {
             alt="Selected"
             className="mt-2 max-w-full rounded-md"
           />
+          <div className="mt-2">
+            <button
+              onClick={removeImage}
+              className="px-4 py-2 bg-emerald-600 text-white rounded-md  hover:ring-2 hover::ring-offset-2 hover::ring-gray-900"
+            >
+              Remove
+            </button>
+          </div>
         </div>
       )}
     </div>
